8dd0b35e0a9cfe80c4f474a0d66d16a9d94bf55c
difcortesgu <difcortesgu@unal.edu.co>
2017-03-03 14:36:13 -0500

    Update Main.java

--- src/histogramas/Main.java
+++ src/histogramas/Main.java
@@ -3,7 +3,6 @@ package histogramas;
 public class Main {
     
     public static void main(String []args){
-        System.out.println("prueba de versiones");
         Patron p1= new Patron();
         Histograma h1= new Histograma(p1);
         h1.repartir();    
